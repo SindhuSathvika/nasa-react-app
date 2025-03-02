fb2ab6ec5ffa0e10484f444b613107936c0cd2c9
Sindhu Sathvika <sindhusathvikan@gmail.com>
2025-03-02 17:14:27 +0530

    video-type

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,7 +2,19 @@ export default function Main(props){
     const {data} =props
     return (
         <div className="imgContainer">
-            <img src={data.hdurl} alt={data.title || "mars-image" } className="bgImage"/>
+            {data.media_type === "image" ? (
+                <img src={data.hdurl || data.url} alt={data.title || "bg-image"} className="bgImage" />
+            ) : data.media_type === "video" ? (
+                <div>
+                <p>
+                    <a href={data.url} target="_blank" rel="noopener noreferrer">
+                    Watch the video here
+                    </a>
+                </p>
+                </div>
+            ) : (
+                <p>Sorry, content is unavailable or unsupported.</p>
+            )}
         </div>
     )
 }
